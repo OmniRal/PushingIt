56e24daec2cf7ee23334be9aaabd94970b832a7f
OmniRal <rahulshivani@omniblot.com>
2026-07-10 00:24:16 -0400

    Color palette updated + menu started

--- Scripts/ReplicatedStorage/SharedModules/Info/ColorPalette.lua
+++ Scripts/ReplicatedStorage/SharedModules/Info/ColorPalette.lua
@@ -1,55 +1,29 @@
--- OmniRal
-
 local ColorPalette = {}
 
-ColorPalette.OmniBlotRed = Color3.fromRGB(255, 118, 107)
-ColorPalette.OmniBlotDarkRed = Color3.fromRGB(173, 71, 63)
-ColorPalette.OmniBlotLightRed = Color3.fromRGB(255, 186, 180)
-ColorPalette.OmniBlotRed2 = Color3.fromRGB(255, 97, 76)
+ColorPalette.OmniBlotRed = {RGB = Color3.fromRGB(255, 118, 107), HEX = Color3.fromHex("#FF766B")}
+ColorPalette.OmniBlotDarkRed = {RGB = Color3.fromRGB(173, 71, 63), HEX = Color3.fromHex("#AD473F")}
+ColorPalette.OmniBlotLightRed = {RGB = Color3.fromRGB(255, 186, 180), HEX = Color3.fromHex("#FFBAB4")}
+ColorPalette.OmniBlotRed2 = {RGB = Color3.fromRGB(255, 97, 76), HEX = Color3.fromHex("#FF614C")}
 
-ColorPalette.OmniBlotBlue = Color3.fromRGB(79, 144, 255)
-ColorPalette.OmniBlotDarkBlue = Color3.fromRGB(42, 87, 163)
-ColorPalette.OmniBlotLightBlue = Color3.fromRGB(152, 190, 255)
-ColorPalette.OmniBlotBlue2 = Color3.fromRGB(67, 199, 255)
+ColorPalette.OmniBlotBlue = {RGB = Color3.fromRGB(79, 144, 255), HEX = Color3.fromHex("#4F90FF")}
+ColorPalette.OmniBlotDarkBlue = {RGB = Color3.fromRGB(42, 87, 163), HEX = Color3.fromHex("#2A57A3")}
+ColorPalette.OmniBlotLightBlue = {RGB = Color3.fromRGB(152, 190, 255), HEX = Color3.fromHex("#98BEFF")}
+ColorPalette.OmniBlotBlue2 = {RGB = Color3.fromRGB(67, 199, 255), HEX = Color3.fromHex("#43C7FF")}
 
-ColorPalette.OmniBlotGreen = Color3.fromRGB(149, 255, 149)
-ColorPalette.OmniBlotDarkGreen = Color3.fromRGB(82, 161, 82)
-ColorPalette.OmniBlotLightGreen = Color3.fromRGB(203, 255, 203)
-ColorPalette.OmniBlotGreen2 = Color3.fromRGB(106, 255, 103)
+ColorPalette.OmniBlotGreen = {RGB = Color3.fromRGB(149, 255, 149), HEX = Color3.fromHex("#95FF95")}
+ColorPalette.OmniBlotDarkGreen = {RGB = Color3.fromRGB(82, 161, 82), HEX = Color3.fromHex("#52A152")}
+ColorPalette.OmniBlotLightGreen = {RGB = Color3.fromRGB(203, 255, 203), HEX = Color3.fromHex("#CBFFCB")}
+ColorPalette.OmniBlotGreen2 = {RGB = Color3.fromRGB(106, 255, 103), HEX = Color3.fromHex("#6AFF67")}
 
-ColorPalette.OmniBlotBlack = Color3.fromRGB(46, 46, 46)
+ColorPalette.OmniBlotBlack = {RGB = Color3.fromRGB(46, 46, 46), HEX = Color3.fromHex("#2E2E2E")}
 
-ColorPalette.JetWhite = Color3.fromRGB(255, 255, 255)
-ColorPalette.JetBlack = Color3.fromRGB(0, 0, 0)
-ColorPalette.PaleRed = Color3.fromRGB(255, 102, 102)
-ColorPalette.DarkRed = Color3.fromRGB(140, 42, 42)
-ColorPalette.JetRed = Color3.fromRGB(255, 0, 0)
-ColorPalette.JetGreen = Color3.fromRGB(0, 255, 0)
-ColorPalette.IconGrey = Color3.fromRGB(125, 125, 125)
+ColorPalette.JetWhite = {RGB = Color3.fromRGB(255, 255, 255), HEX = Color3.fromHex("#FFFFFF")}
+ColorPalette.JetBlack = {RGB = Color3.fromRGB(0, 0, 0), HEX = Color3.fromHex("#000000")}
 
-ColorPalette.Black1 = Color3.fromRGB(35, 35, 35)
-ColorPalette.Black2 = Color3.fromRGB(45, 45, 45)
-ColorPalette.Black3 = Color3.fromRGB(55, 55, 55)
+ColorPalette.ShopGreen = {RGB = Color3.fromRGB(74, 218, 49), HEX = Color3.fromHex("4ADA31")}
+ColorPalette.StatsBlue = {RGB = Color3.fromRGB(236, 101, 43), HEX = Color3.fromHex("#EC652B")}
+ColorPalette.SkillsRed = {RGB = Color3.fromRGB(236, 101, 43), HEX = Color3.fromHex("#EC652B")}
+ColorPalette.SettingsBlack = {RGB = Color3.fromRGB(236, 101, 43), HEX = Color3.fromHex("#EC652B")}
 
-ColorPalette.HealthGreen1 = Color3.fromRGB(210, 255, 225)
-ColorPalette.HealthGreen2 = Color3.fromRGB(92, 250, 134)
-ColorPalette.HealthGreen3 = Color3.fromRGB(72, 194, 153)
-
-ColorPalette.HealthRed1 = Color3.fromRGB(250, 171, 171)
-ColorPalette.HealthRed2 = Color3.fromRGB(250, 86, 86)
-ColorPalette.HealthRed3 = Color3.fromRGB(156, 54, 54)
-
-ColorPalette.AmmoLow = Color3.fromRGB(241, 167, 6)
-ColorPalette.AmmoEmpty = Color3.fromRGB(214, 28, 22)
-
-ColorPalette.UIStoneGrey = Color3.fromRGB(121, 136, 151)
-ColorPalette.UIDarkStoneGrey = Color3.fromRGB(53, 64, 77)
-ColorPalette.UIBlackStoneGrey = Color3.fromRGB(26, 32, 38)
-
-ColorPalette.UISteel = Color3.fromRGB(174, 190, 209)
-
-ColorPalette.RoomPad_Available = Color3.fromRGB(83, 219, 83)
-ColorPalette.RoomPad_BeingUsed = Color3.fromRGB(238, 206, 87)
-ColorPalette.RoomPad_Locked = Color3.fromRGB(91, 100, 107)
 
 return ColorPalette
